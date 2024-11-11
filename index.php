cdf30ed4887e203f3d901bb7527c9a8b152a4b0e
Mantih <grigorievnicita@yandex.ru>
2024-11-11 13:26:20 +0300

    Реализована функция деления

--- index.php
+++ index.php
@@ -6,11 +6,31 @@ echo "3. Выполнить вычитание\n";
 echo "4. Выполнить деление\n";
 echo "5. Возвести число в степень\n";
 
+// Выбор действия
 $choice = readline("Выберите действие: ");
 
 switch ($choice) {
     case 1:
         $num1 = readline("Введите первое число: ");
         $num2 = readline("Введите второе число: ");
+        break;
+    case 2:
+        $result = $num1 + $num2;
+        echo "Результат сложения: $result\n";
+        break;
+    case 3:
+        $result = $num1 - $num2;
+        echo "Результат вычитания: $result\n";
+        break;
+    case 4:
+        if ($num2 != 0) {
+            $result = $num1 / $num2;
+            echo "Результат деления: $result\n";
+        } else {
+            echo "Ошибка: деление на ноль\n";
+        }
+        break;
+    default:
+        echo "Неверный выбор\n";
         break;
 }
