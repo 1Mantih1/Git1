371ca9c5b4dc6a821c7b79abaa143a6802845ee0
Mantih <grigorievnicita@yandex.ru>
2024-11-11 13:24:56 +0300

    Реализована функция сложения

--- index.php
+++ index.php
@@ -6,11 +6,19 @@ echo "3. Выполнить вычитание\n";
 echo "4. Выполнить деление\n";
 echo "5. Возвести число в степень\n";
 
+// Выбор действия
 $choice = readline("Выберите действие: ");
 
 switch ($choice) {
     case 1:
         $num1 = readline("Введите первое число: ");
         $num2 = readline("Введите второе число: ");
+        break;
+    case 2:
+        $result = $num1 + $num2;
+        echo "Результат сложения: $result\n";
+        break;
+    default:
+        echo "Неверный выбор\n";
         break;
 }
